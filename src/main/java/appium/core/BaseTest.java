f53b7f608681d812735625f5dfc693d1cded7538
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-05-18 19:28:29 -0300

    BaseTest refactored

--- src/main/java/appium/core/BaseTest.java
+++ src/main/java/appium/core/BaseTest.java
@@ -1,10 +1,16 @@
 package appium.core;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 
 public class BaseTest {
     @AfterEach
     public void tearDown() {
+        DriverFactory.getDriver().resetApp();
+    }
+
+    @AfterAll
+    static void finishClass() {
         DriverFactory.kilLDriver();
     }
 }
