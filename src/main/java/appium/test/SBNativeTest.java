5921c2372f5c3f103694fc1e9e86af400bbd5c41
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-05-28 20:30:56 -0300

    RealAppTest renamed to SBNativeTest

--- src/main/java/appium/test/SBNativeTest.java
+++ src/main/java/appium/test/SBNativeTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RealAppTest extends BaseTest {
+public class SBNativeTest extends BaseTest {
     private final MenuPage menuPage = new MenuPage();
     private final RealAppPage realAppPage = new RealAppPage();
     private final SBMenuPage sbMenuPage = new SBMenuPage();
